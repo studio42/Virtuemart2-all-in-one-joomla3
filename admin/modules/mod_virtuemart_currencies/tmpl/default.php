50bfff3df4792ea3db53acf3e29bdde736a5261e
studio 42 france <studio.france@gmail.com>
2013-08-05 23:06:10 +0200

    Initial all in one pack commit
    This is the original virtuemart 2 all in one . And are not joomla 3
    compatible !!!

---
+++ admin/modules/mod_virtuemart_currencies/tmpl/default.php
@@ -0,0 +1,15 @@
+<?php // no direct access
+defined('_JEXEC') or die('Restricted access');
+vmJsApi::jQuery();
+vmJsApi::chosenDropDowns();
+?>
+
+<!-- Currency Selector Module -->
+<?php echo $text_before ?>
+<form action="<?php echo JURI::getInstance()->toString(); ?>" method="post">
+
+	<br />
+    <input class="button" type="submit" name="submit" value="<?php echo JText::_('MOD_VIRTUEMART_CURRENCIES_CHANGE_CURRENCIES') ?>" />
+	<br />
+	<?php echo JHTML::_('select.genericlist', $currencies, 'virtuemart_currency_id', 'class="inputbox vm-chzn-select"', 'virtuemart_currency_id', 'currency_txt', $virtuemart_currency_id) ; ?>
+</form>
